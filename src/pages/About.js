32d439fc405bfe4dd29be71f9515de40a564c7e7
Rajesh <rajeshchoudhary47@icloud.com>
2021-10-17 09:46:11 +0530

    Short Bio added for about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -5,6 +5,7 @@ function About() {
     <div className="container">
       <h2>About Page</h2>
       <hr />
+      <h2>This page would be to tell user how to handle the website.</h2>
       <div></div>
     </div>
   );
