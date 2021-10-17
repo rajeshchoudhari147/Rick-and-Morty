16b92efa3ac089fe8a6cdeae42c7f412a94df1d3
Rajesh <rajeshchoudhary47@icloud.com>
2021-10-17 17:38:16 +0530

    Refactored

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useHistory, useRouteMatch } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 
 function Character({ character }) {
   let { id, name, image } = character;
