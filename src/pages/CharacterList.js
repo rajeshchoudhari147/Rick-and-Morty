cb11a9a26b44a17ffd308d8397b5c753d3e985e8
Rajesh <rajeshchoudhary47@icloud.com>
2021-10-17 16:14:47 +0530

    Displayed Character's List

--- src/pages/CharacterList.js
+++ src/pages/CharacterList.js
@@ -1,11 +1,39 @@
 import React from "react";
+import { useState, useEffect } from "react";
+import { API_HUMAN } from "../api/rickandmortyapi";
+import Character from "../components/Character";
 
 function CharacterList() {
+  let [characters, setCharacters] = useState(null);
+
+  useEffect(() => {
+    try {
+      fetch(API_HUMAN)
+        .then((response) => response.json())
+        .then(({ results }) => {
+          if (results && Array.isArray(results)) {
+            setCharacters(results);
+          }
+        })
+        .catch((error) => console.log(error));
+    } catch (e) {
+      console.log(e);
+    }
+  }, []);
+
   return (
     <div className="container">
       <h2>Characters</h2>
       <hr />
-      <div></div>
+      <div className="row">
+        {characters.map((character) => {
+          return (
+            <div className="col s12 m4 l3" key={character.id}>
+              <Character key={character.id} character={character} />
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
