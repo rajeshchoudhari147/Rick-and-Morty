66258d71747118b0f6f39e8e22d6ebb9727232bf
Rajesh <rajeshchoudhary47@icloud.com>
2021-10-17 16:22:05 +0530

    Added Loading Component

---
+++ src/components/Loading.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Loading() {
+  return (
+    <div className="progress">
+      <div className="indeterminate">
+        <h2>Components are being loaded</h2>
+      </div>
+    </div>
+  );
+}
+
+export default Loading;
