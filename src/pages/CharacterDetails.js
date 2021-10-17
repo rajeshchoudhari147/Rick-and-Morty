6a3fdc5e732a94cc2e998a203cebe0250fb54f56
Rajesh <rajeshchoudhary47@icloud.com>
2021-10-17 17:39:06 +0530

    Added Character Details to the page

--- src/pages/CharacterDetails.js
+++ src/pages/CharacterDetails.js
@@ -1,13 +1,31 @@
 import React from "react";
+import { useEffect, useState } from "react";
+import { BASE_API } from "../api/rickandmortyapi";
+import Loading from "../components/Loading";
+import CharDetailsContainer from "../components/CharDetailsContainer";
 
-function CharacterDetails() {
-  return (
-    <div className="container">
-      <h2>Character Details</h2>
-      <hr />
-      <div></div>
-    </div>
-  );
+function CharacterDetails({ match }) {
+  let { id } = match.params;
+  let [charDetails, setCharDetails] = useState(null);
+
+  useEffect(() => {
+    try {
+      fetch(`${BASE_API}/${id}`).then((response) =>
+        response
+          .json()
+          .then((result) => setCharDetails(result))
+          .catch((error) => console.log(error))
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  });
+
+  if (!charDetails) {
+    return <Loading />;
+  }
+
+  return <CharDetailsContainer key={id} character={charDetails} />;
 }
 
 export default CharacterDetails;
